f11a4c0ffd355689753df0ba2f4e9bfa2cd6aa94
Casey Moore <Caseymoore19@gmail.com>
2020-06-16 18:06:48 -0700

    added basic template with player search capability

---
+++ static/api_test.py
@@ -0,0 +1,42 @@
+# from googleapiclient.discovery import build
+#
+# api_key = 'AIzaSyCvjOw9OAQAabyaIhtesjKXtGurpQ40e-g'
+# service = build('youtube', 'v3', developerKey=api_key)
+#
+# request = service.channels().list(
+#     part='statistics',
+#     forUsername='schafer5'
+# )
+#
+# response = request.execute()
+# print(response['viewCount'])
+
+import requests
+import json
+
+# teams_dict = {'philadelphia flyers': 4}
+# team_input = input('Enter a team name: ')
+# team_id = teams_dict[team_input.lower()]
+# payload = {'teamId': team_id}
+
+# with open('c:/Users/casey/python/django_projects/statsy/static/player_data.json',
+#           'r') as json_file:
+#     data = json.load(json_file)
+#
+# data = {k: v.lower() for k, v in data.items()}
+#
+# print(data)
+
+# players = [k for k, v in data.items() if v == "Sebastian Aho"]
+player_data = {}
+for i in range(40000):
+    r = requests.get('https://statsapi.web.nhl.com/api/v1/people/{}'.format(8444000+i))
+    r_data = r.json()
+    if r.status_code == 200:
+        player_data[r_data['people'][0]['id']] = r_data['people'][0]['fullName'].lower()
+
+with open('player_data.json', 'w') as f:
+    json.dump(player_data, f)
+# search_result.append(r_data['people'][0])
+#
+# print(search_result[1])
