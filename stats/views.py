b5364ee51f979491ff9355428a64043386f6acdc
Casey Moore <Caseymoore19@gmail.com>
2020-09-09 11:06:27 -0700

    changes to player_detail.html

--- stats/views.py
+++ stats/views.py
@@ -26,9 +26,9 @@ class PlayerDetailView(generic.DetailView):
     model = Player
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+        context = super(PlayerDetailView, self).get_context_data(**kwargs)
         url = 'https://www.nhl.com/player/{}-{}-{}'.format(
-            self.object.first_name, self.object.last_name, self.object.id)
+            self.first_name, self.last_name, self.id)
         stat_data = pd.read_html(url)
         context['career_stats'] = stat_data
         context['test'] = '1'
